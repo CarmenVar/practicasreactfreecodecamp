0a5a2c1cab89ee5a2a8525927723f5995f7ba09e
Carmen Vargas <trishiavargas@gmail.com>
2023-04-04 11:36:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Testimonio from './componentes/testimonio.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+      <h1>Esto es lo que dicen nuestros alumnos</h1>
+      <Testimonio
+      nombre="Alejandro Sanz"
+      pais="España"
+      imagen="alejo"
+      cargo="Cantautor "
+      empresa="Spotify"
+      testimonio="No me da vergüenza decir
+      Que lo eras todo para mi
+      Pero no puedo admitir
+      Que lo quisiera repetir
+      Tantos momentos solo
+      Impresionante mente solo
+      Perdido en la acera
+      No había ni un rastro tuyo
+      La calle estaba tan desierta
+      La noche despiertaue fui es lo soy, no me da verguenza decir, que lo quisiera repetir" />
+      <Testimonio
+       nombre="Ed Sheeran"
+       pais="Canada"
+       imagen="ed-sheeran"
+       cargo="Cantautor"
+       empresa="Amazon"
+       testimonio="No me da vergüenza decir
+       Que lo eras todo para mi
+       Pero no puedo admitir
+       Que lo quisiera repetir
+       Tantos momentos solo
+       Impresionante mente solo
+       Perdido en la acera
+       No había ni un rastro tuyo
+       La calle estaba tan desierta
+       La noche despiertaue fui es lo soy, no me da verguenza decir, que lo quisiera repetir" />
+       <Testimonio
+       nombre="Lady Gaga"
+       pais="USA"
+       imagen="gaga"
+       cargo="Cantautor"
+       empresa="Globant"
+       testimonio="No me da vergüenza decir
+       Que lo eras todo para mi
+       Pero no puedo admitir
+       Que lo quisiera repetir
+       Tantos momentos solo
+       Impresionante mente solo
+       Perdido en la acera
+       No había ni un rastro tuyo
+       La calle estaba tan desierta
+       La noche despiertaue fui es lo soy, no me da verguenza decir, que lo quisiera repetir" />
+      </div>
     </div>
   );
 }
